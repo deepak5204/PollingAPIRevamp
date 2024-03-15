06989a38b2fae8990e16df3cd6e33827407e938d
deepak_nexg <pdeepak1255@gmail.com>
2024-03-15 14:59:06 +0530

    specify the user role and set set all new user role is user

--- src/modals/User.js
+++ src/modals/User.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         requried: true
+    },
+    role: {
+        type: String,
+        enum: ['USER', "ADMIN"],
+        default: 'USER'
     }
 })
 
