dcaeeb044d682f59bbf0f0d4975fbe04df526955
deepak_nexg <pdeepak1255@gmail.com>
2024-03-18 13:03:40 +0530

    create controller for questions

--- src/controllers/question.js
+++ src/controllers/question.js
@@ -1,13 +1,71 @@
-const Question = require('../modals/Question');
+const Question = require("../modals/Question");
+const Option = require('../modals/Options')
 
-module.exports.createQuestions = async(req, res) => {
-    const { question } = req.body
-    const createdQuestion = await Question.create({
-        question
+module.exports.createQuestions = async (req, res) => {
+  const { question } = req.body;
+  const createdQuestion = await Question.create({
+    question,
+  });
+
+  res.status(201).json({
+    status: true,
+    message: "Question created successfully",
+    createdQuestion,
+  });
+};
+
+//View question
+module.exports.getQuestionById = async (req, res) => {
+  let question = await Question.findById(req.params.id).populate("options");
+  res.status(200).json({
+    message: "Success",
+    data: {
+      question,
+    },
+  });
+};
+
+// view all question
+module.exports.getAllQuestions = async (req, res) => {
+  const questions = await Question.find();
+  res.status(200).json({
+    message: "Success",
+    data: {
+      questions,
+    },
+  });
+};
+
+// Delete question
+module.exports.deleteQuestion = async (req, res) => {
+    let id = req.params.id;
+    let question = await Question.findById(id).populate({
+      path: "options",
+      select: "votes",
     });
 
-    res.status(201).json({
-        status: true,
-        createdQuestion
-    })
-}
+    if(!question){
+        res.status(400).json({
+            status: false,
+            message: 'No question with this array!'
+        })
+    }
+      let options = question.options;
+
+      for (let i = 0; i < options.length; i++) {
+        if (options[i].votes > 0) {
+          return res.status(404).json({
+            data: {
+              message: "question option has some votes, Not Possible to delete",
+            },
+          });
+        }
+      }
+
+      await Option.deleteMany({ question: id });
+      await Question.findByIdAndDelete(id);
+
+      res.status(200).json({
+        message: "Question deleted successfully",
+      });
+};
--- src/routes/question.js
+++ src/routes/question.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { createQuestions } = require('../controllers/question');
+const { createQuestions, getQuestionById, getAllQuestions, deleteQuestion } = require('../controllers/question');
 const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.route('/').post(protect, restrictTo('ADMIN'), createQuestions)
+
+router.get('/:id', getQuestionById);
+
+router.get('/', getAllQuestions)
+
+router.delete('/:id', deleteQuestion);
+
+
 
 module.exports = router ;
