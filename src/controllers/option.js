bd7e9232e7baee06a7da7a62708d8e901e8e30c0
Deepak <pdeepak1255@gmail.com>
2024-07-22 12:51:30 +0530

    error handle in question controller and option controller using try and catch

--- src/controllers/option.js
+++ src/controllers/option.js
@@ -13,89 +13,240 @@ module.exports.createOption = async (req, res) => {
         question: questionId,
       });
       option.link_vote =
-        "http://localhost:5000/v2/option/addvote/" + option.id ;
-      option.save();
+        "http://localhost:5000/v2/option/addvote/" + option.id;
+      await option.save();
       question.options.push(option);
-      question.save(); 
+      await question.save(); 
 
       res.status(200).json({
         status: true,
-        message: "option created",
+        message: "Option created",
         option,
+      });
+    } else {
+      res.status(404).json({
+        status: false,
+        message: "Question not found",
       });
     }
   } catch (err) {
-    console.log("Error : ", err);
-    return;
+    res.status(500).json({
+      status: false,
+      message: "Error creating option",
+      error: err.message,
+    });
   }
 };
 
 // Get option
 module.exports.getOption = async (req, res) => {
-  let optionId = req.params.id;
+  try {
+    let optionId = req.params.id;
+    const option = await Option.findById(optionId);
 
-  const option = await Option.findById(optionId);
-
-  res.status(200).json({
-    message: 'Success',
-    option
-  })
-} 
-
-
-//Update Option
-module.exports.updateOption = async (req, res) => {
-  let optionId = req.params.id;
-  const updateData = req.body
-
-  const option = await Option.findByIdAndUpdate(optionId, updateData, {new: true});
-
-  res.status(200).json({
-    message: 'Successfully updated',
-    option
-  })
-}
-
-//delete an option on the basis of its id
-module.exports.deleteOption = async (req, res) => {
-  let optionId = req.params.id;
-
-  //chek and find option exits or not
-  let option = await Option.findById(optionId);
-
-  // if option present then check for vote
-  // if option has vote then don't delete
-  if (option.votes > 0) {
-    res.status(404).json({
-      data: {
-        message: "Can't delete! It has vote",
-      },
+    if (option) {
+      res.status(200).json({
+        message: 'Success',
+        option,
+      });
+    } else {
+      res.status(404).json({
+        message: 'Option not found',
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: "Error retrieving option",
+      error: err.message,
     });
   }
+};
 
-  // delete option from Question's options array
-  await Question.findByIdAndUpdate(option.question, {
-    $pull: { options: optionId },
-  });
+// Update option
+module.exports.updateOption = async (req, res) => {
+  try {
+    let optionId = req.params.id;
+    const updateData = req.body;
+    const option = await Option.findByIdAndUpdate(optionId, updateData, { new: true });
 
-  // delete option from option
-  await Option.findByIdAndDelete(optionId);
-  res.status(200).json({
-    data: {
+    if (option) {
+      res.status(200).json({
+        message: 'Successfully updated',
+        option,
+      });
+    } else {
+      res.status(404).json({
+        message: 'Option not found',
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: "Error updating option",
+      error: err.message,
+    });
+  }
+};
+
+// Delete an option on the basis of its id
+module.exports.deleteOption = async (req, res) => {
+  try {
+    let optionId = req.params.id;
+    let option = await Option.findById(optionId);
+
+    if (!option) {
+      return res.status(404).json({
+        status: false,
+        message: "Option not found",
+      });
+    }
+
+    if (option.votes > 0) {
+      return res.status(400).json({
+        status: false,
+        message: "Can't delete! It has votes",
+      });
+    }
+
+    await Question.findByIdAndUpdate(option.question, {
+      $pull: { options: optionId },
+    });
+
+    await Option.findByIdAndDelete(optionId);
+    res.status(200).json({
       message: "Option deleted successfully",
-    },
-  });
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: "Error deleting option",
+      error: err.message,
+    });
+  }
 };
 
-//adding vote to an option for particular question
+// Adding vote to an option for a particular question
 module.exports.addVote = async (req, res) => {
-  let optionId = req.params.id;
-  //find option if present then vote to it
-  await Option.findByIdAndUpdate(optionId, { $inc: { votes: 1 } });
+  try {
+    let optionId = req.params.id;
+    await Option.findByIdAndUpdate(optionId, { $inc: { votes: 1 } });
 
-  res.status(200).json({
-    data: {
-      message: "Voted Successfully",
-    },
-  });
+    res.status(200).json({
+      message: "Voted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: "Error adding vote",
+      error: err.message,
+    });
+  }
 };
+
+
+/*
+
+// const Option = require("../modals/Options");
+// const Question = require('../modals/Question');
+
+// module.exports.createOption = async (req, res) => {
+//   try {
+//     let questionId = req.params.id;
+//     let question = await Question.findById(questionId);
+
+//     if (question) {
+//       let option = await Option.create({
+//         content: req.body.content,
+//         votes: req.body.votes,
+//         question: questionId,
+//       });
+//       option.link_vote =
+//         "http://localhost:5000/v2/option/addvote/" + option.id ;
+//       option.save();
+//       question.options.push(option);
+//       question.save(); 
+
+//       res.status(200).json({
+//         status: true,
+//         message: "option created",
+//         option,
+//       });
+//     }
+//   } catch (err) {
+//     console.log("Error : ", err);
+//     return;
+//   }
+// };
+
+// // Get option
+// module.exports.getOption = async (req, res) => {
+//   let optionId = req.params.id;
+
+//   const option = await Option.findById(optionId);
+
+//   res.status(200).json({
+//     message: 'Success',
+//     option
+//   })
+// } 
+
+
+// //Update Option
+// module.exports.updateOption = async (req, res) => {
+//   let optionId = req.params.id;
+//   const updateData = req.body
+
+//   const option = await Option.findByIdAndUpdate(optionId, updateData, {new: true});
+
+//   res.status(200).json({
+//     message: 'Successfully updated',
+//     option
+//   })
+// }
+
+// //delete an option on the basis of its id
+// module.exports.deleteOption = async (req, res) => {
+//   let optionId = req.params.id;
+
+//   //chek and find option exits or not
+//   let option = await Option.findById(optionId);
+
+//   // if option present then check for vote
+//   // if option has vote then don't delete
+//   if (option.votes > 0) {
+//     res.status(404).json({
+//       data: {
+//         message: "Can't delete! It has vote",
+//       },
+//     });
+//   }
+
+//   // delete option from Question's options array
+//   await Question.findByIdAndUpdate(option.question, {
+//     $pull: { options: optionId },
+//   });
+
+//   // delete option from option
+//   await Option.findByIdAndDelete(optionId);
+//   res.status(200).json({
+//     data: {
+//       message: "Option deleted successfully",
+//     },
+//   });
+// };
+
+// //adding vote to an option for particular question
+// module.exports.addVote = async (req, res) => {
+//   let optionId = req.params.id;
+//   //find option if present then vote to it
+//   await Option.findByIdAndUpdate(optionId, { $inc: { votes: 1 } });
+
+//   res.status(200).json({
+//     data: {
+//       message: "Voted Successfully",
+//     },
+//   });
+// };
+
+*/
--- src/controllers/question.js
+++ src/controllers/question.js
@@ -1,89 +1,221 @@
 const Question = require("../modals/Question");
-const Option = require('../modals/Options')
+const Option = require('../modals/Options');
 
 module.exports.createQuestions = async (req, res) => {
-  const { question } = req.body;
-  const createdQuestion = await Question.create({
-    question,
-  });
+  try {
+    const { question } = req.body;
+    const createdQuestion = await Question.create({ question });
 
-  res.status(201).json({
-    status: true,
-    message: "Question created successfully",
-    createdQuestion,
-  });
+    res.status(201).json({
+      status: true,
+      message: "Question created successfully",
+      createdQuestion,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Error creating question",
+      error: error.message,
+    });
+  }
 };
 
-//View question
+// View question
 module.exports.getQuestionById = async (req, res) => {
-  let question = await Question.findById(req.params.id).populate("options");
-  res.status(200).json({
-    message: "Success",
-    data: {
-      question,
-    },
-  });
+  try {
+    let question = await Question.findById(req.params.id).populate("options");
+    res.status(200).json({
+      message: "Success",
+      data: { question },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Error retrieving question",
+      error: error.message,
+    });
+  }
 };
 
-// view all question
+// View all questions
 module.exports.getAllQuestions = async (req, res) => {
-  const questions = await Question.find();
-  res.status(200).json({
-    message: "Success",
-    data: {
-      questions,
-    },
-  });
+  try {
+    const questions = await Question.find();
+    res.status(200).json({
+      message: "Success",
+      data: { questions },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Error retrieving questions",
+      error: error.message,
+    });
+  }
 };
 
-
-
-//Update Question
+// Update question
 module.exports.updateQuestion = async (req, res) => {
-  let questionId = req.params.id;
-  let updateData = req.body;
-  const question = await Question.findByIdAndUpdate(questionId, updateData, {
-    new: true
-  });
+  try {
+    let questionId = req.params.id;
+    let updateData = req.body;
+    const question = await Question.findByIdAndUpdate(questionId, updateData, {
+      new: true,
+    });
 
-  res.status(200).json({
-    message: 'Successfully updated',
-    question
-  })
-
-}
-
+    res.status(200).json({
+      message: "Successfully updated",
+      question,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Error updating question",
+      error: error.message,
+    });
+  }
+};
 
 // Delete question
 module.exports.deleteQuestion = async (req, res) => {
+  try {
     let id = req.params.id;
     let question = await Question.findById(id).populate({
       path: "options",
       select: "votes",
     });
 
-    if(!question){
-        res.status(400).json({
-            status: false,
-            message: 'No question with this array!'
-        })
-    }
-      let options = question.options;
-
-      for (let i = 0; i < options.length; i++) {
-        if (options[i].votes > 0) {
-          return res.status(404).json({
-            data: {
-              message: "question option has some votes, Not Possible to delete",
-            },
-          });
-        }
-      }
-
-      await Option.deleteMany({ question: id });
-      await Question.findByIdAndDelete(id);
-
-      res.status(200).json({
-        message: "Question deleted successfully",
+    if (!question) {
+      return res.status(400).json({
+        status: false,
+        message: "No question with this id!",
       });
+    }
+
+    let options = question.options;
+
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].votes > 0) {
+        return res.status(404).json({
+          data: {
+            message: "Question option has some votes, Not possible to delete",
+          },
+        });
+      }
+    }
+
+    await Option.deleteMany({ question: id });
+    await Question.findByIdAndDelete(id);
+
+    res.status(200).json({
+      message: "Question deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Error deleting question",
+      error: error.message,
+    });
+  }
 };
+
+
+
+
+
+
+/*
+
+
+// const Question = require("../modals/Question");
+// const Option = require('../modals/Options')
+
+// module.exports.createQuestions = async (req, res) => {
+//   const { question } = req.body;
+//   const createdQuestion = await Question.create({
+//     question,
+//   });
+
+//   res.status(201).json({
+//     status: true,
+//     message: "Question created successfully",
+//     createdQuestion,
+//   });
+// };
+
+// //View question
+// module.exports.getQuestionById = async (req, res) => {
+//   let question = await Question.findById(req.params.id).populate("options");
+//   res.status(200).json({
+//     message: "Success",
+//     data: {
+//       question,
+//     },
+//   });
+// };
+
+// // view all question
+// module.exports.getAllQuestions = async (req, res) => {
+//   const questions = await Question.find();
+//   res.status(200).json({
+//     message: "Success",
+//     data: {
+//       questions,
+//     },
+//   });
+// };
+
+
+
+// //Update Question
+// module.exports.updateQuestion = async (req, res) => {
+//   let questionId = req.params.id;
+//   let updateData = req.body;
+//   const question = await Question.findByIdAndUpdate(questionId, updateData, {
+//     new: true
+//   });
+
+//   res.status(200).json({
+//     message: 'Successfully updated',
+//     question
+//   })
+
+// }
+
+
+// // Delete question
+// module.exports.deleteQuestion = async (req, res) => {
+//     let id = req.params.id;
+//     let question = await Question.findById(id).populate({
+//       path: "options",
+//       select: "votes",
+//     });
+
+//     if(!question){
+//         res.status(400).json({
+//             status: false,
+//             message: 'No question with this array!'
+//         })
+//     }
+//       let options = question.options;
+
+//       for (let i = 0; i < options.length; i++) {
+//         if (options[i].votes > 0) {
+//           return res.status(404).json({
+//             data: {
+//               message: "question option has some votes, Not Possible to delete",
+//             },
+//           });
+//         }
+//       }
+
+//       await Option.deleteMany({ question: id });
+//       await Question.findByIdAndDelete(id);
+
+//       res.status(200).json({
+//         message: "Question deleted successfully",
+//       });
+// };
+
+
+*/
