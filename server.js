092fe937f2ad69b57a364a71fc89dd590f52ddc4
Deepak <pdeepak1255@gmail.com>
2024-08-12 12:33:51 +0530

    remove deprecated code

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ dotenv.config({path: './src/config/config.env'})
 const connectDB = require("./src/config/db");
 const express = require("express");
 const routerManager = require("./src/routes/router.manager");
-const cors = require('cors')
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded())
-app.use(cors())
 app.use("/", routerManager);
 
 
