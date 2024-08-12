e95e2b3083b1b16d3590abe1c4e81595c5b199d9
Deepak <pdeepak1255@gmail.com>
2024-08-12 14:21:08 +0530

    handle error while creating option for question

--- src/controllers/option.js
+++ src/controllers/option.js
@@ -29,11 +29,6 @@ module.exports.createOption = async (req, res) => {
         message: "Option created",
         option,
       });
-    } else {
-      res.status(404).json({
-        status: false,
-        message: "Question not found",
-      });
     }
   } catch (err) {
     res.status(500).json({
